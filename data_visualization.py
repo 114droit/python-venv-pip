0c88128e8172ff61afed520070def6fa674fcd82
144droit <indieohren@gmail.com>
2024-10-08 15:43:40 +0200

    edited script to show plot of cubric numbers, added .gitignore

---
+++ data_visualization.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+
+# Beispiel-Daten: Zahlen von 0 bis 9 und ihre Quadrate
+x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+y = [i**2 for i in x]  # Quadratzahlen
+z = [i**3 for i in x]  # Kubikzahlen
+
+# Liniendiagramm erstellen
+plt.plot(x, y, label='Quadratzahlen')
+plt.plot(x, z, label='Kubikzahlen')
+
+# Titel und Achsenbeschriftungen hinzufügen
+plt.title('Quadrat-/Kubikzahlen von 0 bis 9')
+plt.xlabel('Zahl')
+plt.ylabel('3.Potenz')
+
+# Legende anzeigen
+plt.legend()
+
+# Diagramm anzeigen
+plt.show()
+
+# Diagramm speichern
+plt.savefig("quadratkubikplot.png",dpi=200)
